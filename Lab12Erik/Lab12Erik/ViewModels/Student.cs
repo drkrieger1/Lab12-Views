49ab4ce029f3ffccad2b55af23dcfda5636c9c02
drkrieger <eplyushko@gmail.com>
2017-09-27 17:32:32 -0700

    basic build out

---
+++ Lab12Erik/Lab12Erik/ViewModels/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab12Erik.ViewModels
+{
+    public class Student
+    {   
+        //student properties
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Class { get; set; }
+    }
+}
